6d88bc269374436a27ddeaaad85bf1c9650666b6
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-09-16 17:11:11 +0000

    hide db url

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect("mongodb://loongluangkhot:Nalongsak085620@ds255797.mlab.com:55797/todo-app-loongluangkhot", { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 mongoose.Promise = Promise;
 
